876b71f3e9660cbb077f9f063e7836983c520bb5
Thanasis Politis <th.politis@7linternational.com>
2017-10-31 14:22:11 +0200

    remove index add dummy endpoint

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ from worker import conn
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    """Index."""
+    return "hello"
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
