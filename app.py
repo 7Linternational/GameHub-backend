dea02c22a42e2cb3e454514774f8048776e0e039
Thanasis Politis <th.politis@7linternational.com>
2017-10-31 14:13:39 +0200

    cleanup

--- app.py
+++ app.py
@@ -1,36 +1,10 @@
 """Flask main file."""
 
-import os
 from flask import Flask
-# from flask import Response
-from flask import render_template
 from rq import Queue
 from worker import conn
-from testing.Testing import test_commitment_api
-from testing.Testing import check_test
 
 app = Flask(__name__)
-q = Queue(os.environ['QUEUE_NAME'], connection=conn)
-
-
-@app.route('/')
-def index():
-    """Testing page."""
-    return render_template('index.html')
-
-
-@app.route('/testing', methods=['GET', 'POST'])
-def testing():
-    """Testing endpoint."""
-    res = q.enqueue(test_commitment_api, timeout=7200)
-    print(res)
-    return "Test started"
-
-
-@app.route('/check', methods=['GET'])
-def check():
-    """Check."""
-    return check_test()
 
 
 if __name__ == '__main__':
