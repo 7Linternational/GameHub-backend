ba6f672880114c2aee0c5db722e49635c9a1a8e3
Thanasis Politis <th.politis@7linternational.com>
2017-10-31 15:56:30 +0200

    adding missing required files for mod_wsgi

---
+++ gamehub/__init__.py
@@ -0,0 +1,16 @@
+"""Flask main file."""
+
+from flask import Flask
+from rq import Queue
+from worker import conn
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Index."""
+    return "hello"
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
